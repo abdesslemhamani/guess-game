026cbcb2c3b9af1b7236d0340f709101c8a7cbdd
abdesslemhamani <abdesslemhamani@gmail.com>
2025-03-17 03:37:03 +0100

    inicial commit in js

---
+++ main.js
@@ -0,0 +1,56 @@
+let gamename = "Guess the word";
+document.title = gamename;
+document.querySelector("h1").innerHTML = gamename;
+document.querySelector("footer").innerHTML = `${gamename} game created by abdesslem hamani`;
+
+
+let numberoftries = 6;
+let numberofletters = 6;
+let currentetry = 1;
+
+function generateinput(){
+    const inputscontainer = document.querySelector(".inputs");
+
+    for (let i = 1 ; i<= numberoftries ; i++){
+        const trydiv = document.createElement("div");
+        trydiv.classList.add(`try-${i}`);
+        trydiv.innerHTML=`<span>try${i}</span>`;
+        if (i !== 1 ) trydiv.classList.add("disabled-inputs");
+        for (let j = 1 ; j<= numberofletters ; j++){
+const input = document.createElement("input");
+input.type = "text";
+input.id =`guess-${i}-letter-${j}`;
+input.setAttribute("maxlength","1");
+trydiv.appendChild(input)
+        }
+        inputscontainer.appendChild(trydiv);
+    }
+}
+window.onload = function(){
+    generateinput();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
